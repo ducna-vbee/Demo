9192554702e53b8230742666b7e1574a538ce95d
Nguyễn Anh Đức <126655979+ducna-vbee@users.noreply.github.com>
2026-04-08 18:31:55 +0700

    feat: modified index.js and index.ts

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function index()
 {
+    const data = { id: 1, name: 'Alice', role: 'administrator' };
 
+    check(data);
 }
 
 index();
--- index.ts
+++ index.ts
@@ -14,8 +14,6 @@ function isAdmin(person: any) : person is Administrator
 	return (person !== null) && (typeof person === 'object') && ('role' in person) && (person.role === 'admin');
 }
 
-const data: any = { id: 1, name: 'Alice', role: 'administrator' };
-
 function check(data : User)
 {
 	if (isAdmin(data) === true)
@@ -27,5 +25,3 @@ function check(data : User)
 		console.log("Not an administrator!");
 	}
 }
-
-check(data);
