12595470afca3104804a29ef6fd6c5e659c80974
Nguyễn Anh Đức <126655979+ducna-vbee@users.noreply.github.com>
2026-04-08 18:29:51 +0700

    feat: added index.ts index.js

---
+++ index.ts
@@ -0,0 +1,31 @@
+interface User
+{
+	id: number;
+	name: string;
+}
+
+interface Administrator extends User
+{
+	role: 'administrator';
+}
+
+function isAdmin(person: any) : person is Administrator
+{
+	return (person !== null) && (typeof person === 'object') && ('role' in person) && (person.role === 'admin');
+}
+
+const data: any = { id: 1, name: 'Alice', role: 'administrator' };
+
+function check(data : User)
+{
+	if (isAdmin(data) === true)
+	{
+		console.log(`Administrator Role: ${data.role}`);
+	}
+	else
+	{
+		console.log("Not an administrator!");
+	}
+}
+
+check(data);
